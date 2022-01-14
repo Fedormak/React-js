20bac43cba767388ae5aa20f1ea34c2e471d8b66
Fedormak <fodormakarov@gmail.com>
2022-01-15 00:16:43 +0300

    migration

--- Projects/1-first/my-app/src/redux/users-reduser.js
+++ Projects/1-first/my-app/src/redux/users-reduser.js
@@ -1,24 +1,29 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
- 
-
 
 let ininitialState = {
     users: [
-        {id:1,followed: false, fullName:"Dmirty", status:"I'm like snowdording", location: {country:"Belarus",city: "Minsk"}},
-        {id:2,followed: true, fullName:"Daria", status:"I'm listening music now", location: {country:"Moscow",city: "Russia"}},
-        {id:3,followed: false, fullName:"Max" ,status:"I loved, I love, I will love tea!!", location: {country:"Kiev",city: "Ukraine"}},
-        {id:4,followed: true, fullName:"Yan", status:"I am very happy!!", location: {country:"Netherlands",city: "Amsterdam"}}
+        { id: 1, followed: false, fullName: "Dmirty", status: "I'm like snowdording", location: { country: "Belarus", city: "Minsk" } },
+        { id: 2, followed: true, fullName: "Daria", status: "I'm listening music now", location: { country: "Moscow", city: "Russia" } },
+        { id: 3, followed: false, fullName: "Max", status: "I loved, I love, I will love tea!!", location: { country: "Kiev", city: "Ukraine" } },
+        { id: 4, followed: true, fullName: "Yan", status: "I am very happy!!", location: { country: "Netherlands", city: "Amsterdam" } }
     ]
 }
 
 const usersReducer = (state = ininitialState, action) => {
 
+    switch (action.type) {
+        case FOLLOW:
+            let stateCopy = { ...state, users: state.users.map( users => users )}
+        case UNFOLLOW:
 
+        default:
+            break;
+    }
 }
 
-export const followAC = () => ({type: FOLLOW })
-export const unfollowAC = () => ({type: UNFOLLOW })
+export const followAC = (userID) => ({ type: FOLLOW, userID })
+export const unfollowAC = (userID) => ({ type: UNFOLLOW, userID })
 
 
 export default usersReducer
